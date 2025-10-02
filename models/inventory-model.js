f15474fa07e88611a69f98884b8ff31e079ab19f
Brandon-Mount <bmount44@gmail.com>
2025-10-02 08:26:31 -0600

    made a fix

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,10 +1,13 @@
+// models/inventory-model.js
 const pool = require("../database/")
 
 /* ***************************
  *  Get all classification data
  * ************************** */
-async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+async function getClassifications() {
+  return await pool.query(
+    "SELECT * FROM public.classification ORDER BY classification_name"
+  )
 }
 
 /* ***************************
@@ -14,33 +17,35 @@ async function getInventoryByClassificationId(classification_id) {
   try {
     const data = await pool.query(
       `SELECT * FROM public.inventory AS i 
-      JOIN public.classification AS c 
-      ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = $1`,
+       JOIN public.classification AS c 
+       ON i.classification_id = c.classification_id 
+       WHERE i.classification_id = $1`,
       [classification_id]
     )
     return data.rows
   } catch (error) {
-    console.error("getclassificationsbyid error " + error)
+    console.error("getInventoryByClassificationId error", error)
   }
 }
 
-// models/inventory-model.js
-const pool = require("../database/");
-
+/* ***************************
+ *  Get a specific vehicle by inv_id
+ * ************************** */
 async function getVehicleById(inv_id) {
   try {
     const result = await pool.query(
       "SELECT * FROM inventory WHERE inv_id = $1",
       [inv_id]
-    );
-    return result.rows[0];
+    )
+    return result.rows[0]
   } catch (error) {
-    console.error("getVehicleById error:", error);
+    console.error("getVehicleById error:", error)
   }
 }
 
+module.exports = {
+  getClassifications,
+  getInventoryByClassificationId,
+  getVehicleById,
+}
 
-
-
-module.exports = {getClassifications, getInventoryByClassificationId, getVehicleById}
